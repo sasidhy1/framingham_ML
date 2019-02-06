52a7b08d338d80cb2fe9095a65a44c34085709b4
khrysvaughan <39607183+khrysvaughan@users.noreply.github.com>
2019-02-05 21:15:38 -0500

    Updated with all model details

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ import sys
 import keras
 from keras.models import load_model
 import tensorflow as tf
+from joblib import load
+import os
+
 
 
 app = Flask(__name__)
@@ -13,7 +16,9 @@ app = Flask(__name__)
 # Model Holders
 model = None
 
-stroke_model = load_model("[13-14-7-1]_1000e_1b.h5")
+stroke_model = load_model("framingham_stroke.h5")
+cvd_model = load_model("framingham_cvd.h5")
+angina_model = load_model("farmingham_angina.h5")
 graph = tf.get_default_graph()
 
 
@@ -86,28 +91,80 @@ def test_model_prediction():
 
     global graph
     with graph.as_default():
-        pt = {'SEX':[sex],'AGE':[age],'CIGPDAY':[cigsperday],'HEARTRTE':[heartrate],'SYSBP':[systolic],'BPMEDS':[bp],'TOTCHOL':[cholesterol],'BMI':[bmi],'GLUCOSE':[glucose],'DIABETES':[dia],'EDUC_2.0':[EDUC_2],'EDUC_3.0':[EDUC_3],'EDUC_4.0':[EDUC_4]}
-        good_patient = pd.DataFrame(pt)
+        stroke_data = {'SEX':[sex],'AGE':[age],'CIGPDAY':[cigsperday],'HEARTRTE':[heartrate],'SYSBP':[systolic],'BPMEDS':[bp],'TOTCHOL':[cholesterol],'BMI':[bmi],'GLUCOSE':[glucose]}
+        #'DIABETES':[dia],'EDUC_2':[EDUC_2],'EDUC_3':[EDUC_3],'EDUC_4':[EDUC_4]
+        scaled_stroke_pt = {}
+        scaled_angina_pt = {}
+        scaled_cvd_pt = {}
+
+
+        for key in stroke_data:
+            if f'{key}_scaler.joblib' in os.listdir('scalers'):
+                scaler = load(f'scalers/{key}_scaler.joblib')
+                a = scaler.transform(stroke_data[key][0])
+                scaled_stroke_pt[key] = [a[0,0]]
+
+        good_patient = pd.DataFrame(scaled_stroke_pt)
 
         # Use the model to make a prediction
-        results = stroke_model.predict_proba(good_patient)[0,0]
-    
-    print(results)
+        stroke_results = stroke_model.predict_proba(good_patient)[0,0]
+
+        cvd_data = {'SEX':[sex],'AGE':[age],'CIGPDAY':[cigsperday],'HEARTRTE':[heartrate],'SYSBP':[systolic],'TOTCHOL':[cholesterol],'BMI':[bmi],'GLUCOSE':[glucose]}
+
+        for key in cvd_data:
+            if f'{key}_scaler.joblib' in os.listdir('scalers'):
+                scaler = load(f'scalers/{key}_scaler.joblib')
+                a = scaler.transform(cvd_data[key][0])
+                scaled_cvd_pt[key] = [a[0,0]]
+
+        cvd_patient = pd.DataFrame(scaled_cvd_pt)
+        cvd_results = cvd_model.predict_proba(cvd_patient)[0,0]
+
+        angina_data = {'SEX':[sex],'AGE':[age], 'TOTCHOL':[cholesterol], 'BMI':[bmi], 'HEARTRTE':[heartrate],'SYSBP':[systolic]}
+
+        for key in angina_data:
+            if f'{key}_scaler.joblib' in os.listdir('scalers'):
+                scaler = load(f'scalers/{key}_scaler.joblib')
+                a = scaler.transform(angina_data[key][0])
+                scaled_angina_pt[key] = [a[0,0]]
+
+        angina_patient = pd.DataFrame(scaled_angina_pt)
+        angina_results = angina_model.predict_proba(angina_patient)[0,0]
+
+    print(stroke_results)
+    print(cvd_results)
+    print(angina_results)
+
+    # Regression Model Results
+    reg_diabetes = -14.162026063 + ( 0.058979722 * float(age)) + ( 0.058252121 * float(bmi) ) + (0.051667194 * float(glucose)) + (0.009398841 * float(systolic)) - (0.379299003 * float(sex))
+    reg_stroke = -7.918387257 + (0.057798562 * float(age)) + (0.012255363 * float(cigsperday)) - (0.002613120 * float(cholesterol)) + (0.023016992 * float(bmi)) - (0.005584217 * float(heartrate)) + ( 0.020128967 * float(systolic)) - ( 0.149489784 * float(sex))
+    reg_angina = -5.679135058 + (0.025249146 * float(age)) + (0.005638028 * float(cholesterol)) + (0.031917873 * float(bmi)) - (0.006081201 * float(heartrate)) +  (0.008504367 * float(systolic)) - (0.548017154 * float(sex))
+    reg_cvd = -7.165683847 + (0.044048379 * float(age)) + (0.009451579 * float(cigsperday)) + (0.003237465 * float(cholesterol)) + (0.031233422 * float(bmi)) + (0.004548587 * float(glucose)) - (0.005103114 * float(heartrate)) + (0.017607407 * float(systolic)) - (0.981634642 * float(sex))
+
+    final_diabetes = 100 * (np.exp(reg_diabetes) / (1 + np.exp(reg_diabetes)))
+    final_stroke = 100 * (np.exp(reg_stroke) / (1 + np.exp(reg_stroke)))
+    final_angina = 100 * (np.exp(reg_angina) / (1 + np.exp(reg_angina)))
+    final_cvd = 100 * (np.exp(reg_cvd) / (1 + np.exp(reg_cvd)))
+
+    print(reg_diabetes)
+    print(reg_stroke)
+    print(reg_angina)
+    print(reg_cvd)
+
+    print(final_diabetes)
+    print(final_stroke)
+    print(final_angina)
+    print(final_cvd)
 
     # return f"Risk of stroke: {results}<br><a href='/'>resulsforheart</a>"
-    return redirect(url_for('resultsforheart', stroke=results))
+    return redirect(url_for('resultsforheart', stroke=stroke_results, cvd=cvd_results, angina=angina_results, diabetes=final_diabetes, regstroke=final_stroke, regangina=final_angina, regcvd=final_cvd))
 
 
-@app.route("/resultsforheart/<stroke>")
-def resultsforheart(stroke):
+@app.route("/resultsforheart/<stroke>/<cvd>/<angina>/<diabetes>/<regstroke>/<regangina>/<regcvd>")
+def resultsforheart(stroke, cvd, angina,diabetes,regstroke,regangina,regcvd):
     """Return the predictions of heart conditions."""
-    return render_template("results.html", stroke=stroke)
+    return render_template("results.html", stroke=stroke, cvd=cvd, angina=angina,diabetes=diabetes,regstroke=regstroke,regangina=regangina,regcvd=regcvd)
 
-
-@app.route("/diabetes")
-def compare():
-    """Return the prediction for diabetes."""
-    return render_template("diabetes.html")
 
 
 if __name__ == "__main__":
